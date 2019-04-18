a083cb1381cb1e3c9efc801c4f879164d7bbdd78
Carlos Calderon <cdcalderon@gmail.com>
2019-04-18 03:06:39 -0500

    add scheduler to simulate broker event every 5 seconds

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+var schedule = require("node-schedule");
 
 app.use("/style", express.static(__dirname + "/style"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
-
+let job = null;
 io.on("connection", socket => {
   socket.on("message", msg =>
     io.emit("message", { system: socket.appid, message: msg })
@@ -17,6 +18,17 @@ io.on("connection", socket => {
       system: socket.appid,
       message: socket.appid + " has joined!"
     });
+
+    if (job == null) {
+      job = schedule.scheduleJob(" */5 * * * * *", function() {
+        io.emit("new-broker-message", {
+          system: "Rabbit MQ new Message",
+          message:
+            "Broker was just updated, this is the new message .." +
+            new Date().toLocaleString()
+        });
+      });
+    }
 
     console.log(socket.appid + " has joined!");
   });
